01f781ddff531a7415ca94cee70ba341e4d62b20
Tanvir Emon <tanvirhasan.th157@gmail.com>
2023-03-15 22:10:13 +0600

    First React App

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+let myName = "Tanvir Hasan Emon Khan Khan Khan Khan";
+function App() {
+  return (
+    <div className="name">
+      <h1>My name is {myName}</h1>
+      <p>I am a software engineer.</p>
+    </div>
+  );
+}
+
+export default App;
